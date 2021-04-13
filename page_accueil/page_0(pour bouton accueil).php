460775ff918984d0cd9dcc2f8521896134852038
Xin-YAO1 <72502557+Xin-YAO1@users.noreply.github.com>
2021-04-13 13:43:23 +0200

    Create page_0(pour bouton accueil).php

---
+++ page_accueil/page_0(pour bouton accueil).php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+
+<html>
+
+  <head>
+    <title></title>
+    <meta content="info">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="page_0.css" />
+	<style>
+		h1 {text-align: center;font-size:38px;}
+		p{font-size:20px;}
+	</style>
+  </head>
+  
+  <body>
+  <h1>Bienvenue sur le site de gestion des entretiens d'entrée à Polytech Annecy-Chambéry</h1>
+  <p> Ce site est pour réaliser le projet de INFO642-Base de données et technologies web.Chaque année, à diverses occasions, des entretiens sont organisés au sein de Polytech Annecy-Chambéry :
+  </p>
+  <ul style="font-size:18px;">
+  <li>Les entretiens de recrutements (PEIP ou filière ingénieurs)</li>
+  <li>Les évaluations de compétences des étudiants dans le cadre des APP</li>
+  <li>Les évaluation de projets (projet usage, recherche & développement, ...)</li>
+  <li>Les évaluation de stages (stage ingénieur, ...)</li>
+  </ul>  
+  <p>Ces sessions d'entretiens mobilisent à chaque fois plusieurs ressources :</p>
+  <ul style="font-size:18px;">
+  <li>des étudiants ou futurs étudiants</li>
+  <li>des enseignants</li>
+  <li>des salles</li>
+  </ul> 
+<p>Une session est généralement organisée sur plusieurs demi-journées. Les membres du jury sont bloqués sur des demi-journées entières selon leurs disponibilités. Le jury se compose d'au moins 2 enseignants. Les étudiants eux sont convoqués sur une plage horaire précise.</p>  
+<p>Après chaque entretien, chaque membre du jury réalise individuellement une évaluation qu'il saisit dans une grille composée de critères. L'expression des critères ainsi que  leur niveau d’évaluation peut différer selon les types de session.</p><br>  
+<img src="Woofy.jpg" width="px" height="px" style=" position:absolute;left:550px;top:px;"> 
+<img src="reu1.jpg" width="798.3px" height="400px" style=" position:absolute;right:100px;top:px;"> 
+  </body>
+</html>  
+  
+  
+  
