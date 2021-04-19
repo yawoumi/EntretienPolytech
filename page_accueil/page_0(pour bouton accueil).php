2e77852e6d510b9585e0b24bc30c7324b890faa7
Xin-YAO1 <72502557+Xin-YAO1@users.noreply.github.com>
2021-04-20 01:12:46 +0200

    Update page_0(pour bouton accueil).php

--- page_accueil/page_0(pour bouton accueil).php
+++ page_accueil/page_0(pour bouton accueil).php
@@ -10,13 +10,16 @@
 	<style>
 		h1 {text-align: center;font-size:38px;}
 		p{font-size:20px;}
-		
+		.imag{height: 350px;}
+    
+
 		
 	</style>
   </head>
   
   <body>
-  <h1>Bienvenue sur le site de gestion des entretiens d'entrée à Polytech Annecy-Chambéry</h1>
+  <h1>Bienvenue sur le site de gestion des entretiens d'entrée à </h1>
+   <h1>  Polytech Annecy-Chambéry</h1>
   <p> Ce site est pour réaliser le projet de INFO642-Base de données et technologies web.Chaque année, à diverses occasions, des entretiens sont organisés au sein de Polytech Annecy-Chambéry :
   </p>
   <ul style="font-size:18px;">
@@ -34,10 +37,9 @@
 <p>Une session est généralement organisée sur plusieurs demi-journées. Les membres du jury sont bloqués sur des demi-journées entières selon leurs disponibilités. Le jury se compose d'au moins 2 enseignants. Les étudiants eux sont convoqués sur une plage horaire précise.</p>  
 <p>Après chaque entretien, chaque membre du jury réalise individuellement une évaluation qu'il saisit dans une grille composée de critères. L'expression des critères ainsi que  leur niveau d’évaluation peut différer selon les types de session.</p><br>  
 <div class="imag">
-	<img id="imgR1" src="reu11.jpg" > 
-	<img id="imgW" src="Woofy.jpg" > 
-	<img id="imgR" src="reu1.jpg" >
-	
+  
+  <img  style="height:310px"  src="Woofy.jpg" > 
+  <img  style="height:310px"  src="reu1.jpg" > 	
 </div>
 
   </body>
